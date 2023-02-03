88610d48b8475ccf3d4e458242db02df7b0aa9ab
EkaanshArora <ekaansh68@gmail.com>
2023-02-03 17:35:17 +0530

    fix: remove videocall

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 const Home: NextPage = () => {
   return (
     <>
-      <Link href={'/call/test'}>call</Link>
       <Link href={'/fiber'}>fiber</Link>
     </>
   );
