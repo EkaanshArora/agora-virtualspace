b7a69b51177eefb52c5f8ebde8871d2ad93b3790
EkaanshArora <ekaansh68@gmail.com>
2023-02-03 18:55:12 +0530

    feat: animate character movement

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,17 @@
 import { useFrame } from "@react-three/fiber";
 import { useKeyboardControls } from "@react-three/drei";
-import type { Dispatch, SetStateAction } from "react";
+import { Dispatch, MutableRefObject, SetStateAction, useState } from "react";
 import { useRef } from "react";
-import type { Sprite } from "three";
-import { TextureLoader } from "three";
+import type { Sprite} from "three";
+// import { TextureLoader } from "three";
 import { Vector3 } from "three";
 import type { Controls } from "./types";
 import { rtmChannel } from "./Fiber";
+import { useAnimatedSprite } from 'use-animated-sprite';
+
+const charSize = 1.2;
+const _velocity = new Vector3();
+const speed = 1;
 
 export const sendPositionRTM = (position: Vector3) => {
   void rtmChannel
@@ -14,29 +19,41 @@ export const sendPositionRTM = (position: Vector3) => {
     .catch((e) => console.log(e));
 };
 
-const playerTexMap = new TextureLoader().load("/char.png");
-const charSize = 0.5;
-const _velocity = new Vector3();
-const speed = 4;
-
 export const Player = (props: {
   setPlayerPos: Dispatch<SetStateAction<Vector3>>;
 }) => {
   const ref = useRef<Sprite>(null);
+  const [s, _ss] = useState(spriteConfigs.left)
+  const texture = useAnimatedSprite(ref as MutableRefObject<Sprite>, s)
   const counter = useRef(0);
   const { setPlayerPos } = props;
   const [, get] = useKeyboardControls<Controls>();
   useFrame((s, dl) => {
     if (!ref.current) return;
     const state = get();
-    if (state.left && !state.right) _velocity.x = -1;
-    if (state.right && !state.left) _velocity.x = 1;
-    if (!state.left && !state.right) _velocity.x = 0;
-
-    if (state.forward && !state.back) _velocity.y = 1;
-    if (state.back && !state.forward) _velocity.y = -1;
+    if (state.left && !state.right) {
+      _velocity.x = -1;
+      _ss(spriteConfigs.left)
+    }
+    if (state.right && !state.left) {
+      _velocity.x = 1;
+      _ss(spriteConfigs.right)
+    }
+    if (!state.left && !state.right) {
+      _velocity.x = -0;
+      _ss(spriteConfigs.stand)
+    }
+    
+    if (state.forward && !state.back){
+      _velocity.y = 1;
+      _ss(spriteConfigs.up)
+    }
+    if (state.back && !state.forward){
+      _velocity.y = -1;
+      _ss(spriteConfigs.down)
+    }
     if (!state.forward && !state.back) _velocity.y = 0;
-
+    
     if (state.jump) ref.current.position.set(0, 0, 0);
     ref.current.position.addScaledVector(_velocity, speed * dl);
     setPlayerPos(ref.current.position);
@@ -47,10 +64,58 @@ export const Player = (props: {
       counter.current = Math.round(time * factor) / factor;
     }
   });
-
+  
   return (
     <sprite ref={ref} scale={charSize}>
-      <spriteMaterial map={playerTexMap} />
+      <spriteMaterial map={texture} />
     </sprite>
   );
 };
+
+const spriteConfigs = {
+  stand:{
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 1,
+    spriteX: 0,
+    spriteY: 7,
+    interval: 0.2,
+  },
+  left:{
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 6,
+    spriteX: 0,
+    spriteY: 0,
+    interval: 0.2,
+  },
+  right:{
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 6,
+    spriteX: 0,
+    spriteY: 1,
+    interval: 0.2,
+  },
+  up:{
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 6,
+    spriteX: 0,
+    spriteY: 2,
+    interval: 0.2,
+  },
+  down: {
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 6,
+    spriteX: 0,
+    spriteY: 3,
+    interval: 0.2,
+  }
+}
--- src/components/RemoteSprite.tsx
+++ src/components/RemoteSprite.tsx
@@ -1,13 +1,12 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
-import { TextureLoader } from "three";
+import type { MutableRefObject} from "react";
+import { useRef, useState } from "react";
 import type { Vector3, Sprite } from "three";
 import { AgoraDict, rtcClient } from "./Fiber";
+import { useAnimatedSprite } from "use-animated-sprite";
 
 const distanceToUnsubscribe = 1;
-
-const remotePlayerTexMap = new TextureLoader().load("/char.png");
-const charSize = 0.5;
+const charSize = 1.2;
 
 export const RemoteSprite = (props: {
   position: Vector3;
@@ -15,12 +14,41 @@ export const RemoteSprite = (props: {
   uid: number;
 }) => {
   const ref = useRef<Sprite>(null);
+  const [s, _ss] = useState(spriteConfigs.left);
+  const texture = useAnimatedSprite(ref as MutableRefObject<Sprite>, s);
   const { position, playerPos, uid } = props;
   useFrame(() => {
     const remotePos = ref.current?.position;
     const agoraUser = AgoraDict[uid];
     if (remotePos) {
-      remotePos.lerp(position, 0.1);
+      remotePos.lerp(position, 0.5);
+      // spritesheet logic
+      if (
+        Math.abs(position.x - remotePos.x) < 1e-4 &&
+        Math.abs(position.y - remotePos.y) < 1e-4
+      ) {
+        _ss(spriteConfigs.stand);
+      } else if (
+        Math.abs(position.x - remotePos.x) > Math.abs(position.y - remotePos.y)
+      ) {
+        if (position.x > remotePos.x) {
+          _ss(spriteConfigs.right);
+          console.log("right");
+        } else if (position.x < remotePos.x) {
+          console.log("left");
+          _ss(spriteConfigs.left);
+        }
+      } else {
+        if (position.y > remotePos.y) {
+          console.log("up");
+          _ss(spriteConfigs.up);
+        } else if (position.y < remotePos.y) {
+          console.log("down");
+          _ss(spriteConfigs.down);
+        }
+      }
+
+      // subscription logic
       if (agoraUser) {
         if (
           remotePos.distanceTo(playerPos) > distanceToUnsubscribe &&
@@ -53,7 +81,55 @@ export const RemoteSprite = (props: {
   });
   return (
     <sprite ref={ref} scale={charSize}>
-      <spriteMaterial map={remotePlayerTexMap} />
+      <spriteMaterial map={texture} />
     </sprite>
   );
+};
+
+const spriteConfigs = {
+  stand: {
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 1,
+    spriteX: 0,
+    spriteY: 7,
+    interval: 0.2,
+  },
+  left: {
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 6,
+    spriteX: 0,
+    spriteY: 0,
+    interval: 0.2,
+  },
+  right: {
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 6,
+    spriteX: 0,
+    spriteY: 1,
+    interval: 0.2,
+  },
+  up: {
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 6,
+    spriteX: 0,
+    spriteY: 2,
+    interval: 0.2,
+  },
+  down: {
+    spriteSheetUrl: `/charh.png`,
+    xCount: 8,
+    yCount: 8,
+    spriteFrames: 6,
+    spriteX: 0,
+    spriteY: 3,
+    interval: 0.2,
+  },
 };
