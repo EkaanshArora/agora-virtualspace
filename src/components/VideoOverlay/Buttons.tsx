565446c8fe0aba233eaa99b7d09b8c08c7e2c455
EkaanshArora <ekaansh68@gmail.com>
2023-03-13 16:52:39 +0530

    feat: device selection

--- src/components/VideoOverlay/Buttons.tsx
+++ src/components/VideoOverlay/Buttons.tsx
@@ -2,40 +2,82 @@ import type { ICameraVideoTrack, IMicrophoneAudioTrack } from "agora-rtc-sdk-ng"
 import AgoraRTC from "agora-rtc-sdk-ng";
 import { useState } from "react";
 import { FiCamera, FiCameraOff, FiMic, FiMicOff } from "react-icons/fi";
-
+const cams = await AgoraRTC.getCameras();
+const mics = await AgoraRTC.getMicrophones();
 export function Buttons(props: {
   localVideoMuteState: boolean;
-  setLocalVideoMuteState: React.Dispatch<boolean>
+  setLocalVideoMuteState: React.Dispatch<boolean>;
   localVideoTrack: ICameraVideoTrack;
   localAudioTrack: IMicrophoneAudioTrack;
 }) {
-  const { localAudioTrack, localVideoTrack, localVideoMuteState, setLocalVideoMuteState} = props;
-  const [s1, ss1] = useState(false);
+  const { localAudioTrack, localVideoTrack, localVideoMuteState, setLocalVideoMuteState } = props;
+  const [localAudioMuteState, setLocalAudioMuteState] = useState(false);
+  const [currentCam, setCurrentCam] = useState<string>(cams[0]?.deviceId as string);
+  const [currentMic, setCurrentMic] = useState<string>(mics[0]?.deviceId as string);
+
   const localVideoTrackMute = () => {
-    void localVideoTrack.setMuted(!localVideoTrack.muted).then(() => setLocalVideoMuteState(localVideoTrack.muted));
+    void localVideoTrack
+      .setMuted(!localVideoTrack.muted)
+      .then(() => setLocalVideoMuteState(localVideoTrack.muted));
   };
+
   const localAudioTrackMute = () => {
-    void localAudioTrack.setMuted(!localAudioTrack.muted).then(() => ss1(localAudioTrack.muted));
+    void localAudioTrack
+      .setMuted(!localAudioTrack.muted)
+      .then(() => setLocalAudioMuteState(localAudioTrack.muted));
   };
+
   return (
-    <div className="absolute bottom-0 right-4 z-20 bg-white bg-opacity-80 p-5">
-      <button
-        onClick={() => {
-          void AgoraRTC.getCameras().then((d) => {
-            if (d[1]) void localVideoTrack.setDevice(d[1].deviceId);
-          });
-        }}
-      >
-        cam
-      </button>
-      <br />
-      <button className="h-10" onClick={() => localVideoTrackMute()}>
-        {localVideoMuteState ? <FiCameraOff size={25} /> : <FiCamera size={25} />}
-      </button>
-      <br />
-      <button className="h-10" onClick={() => localAudioTrackMute()}>
-        {s1 ? <FiMicOff size={25} /> : <FiMic size={25} />}
-      </button>
+    <div className="absolute bottom-2 right-2 z-20 rounded-lg bg-white bg-opacity-80 p-4">
+      <div className="flex flex-row">
+        <button className="mr-2 h-8" onClick={() => localVideoTrackMute()}>
+          {localVideoMuteState ? <FiCameraOff size={25} /> : <FiCamera size={25} />}
+        </button>
+        <select
+          id="cam"
+          className="block h-8 w-56 rounded-lg border border-gray-300 bg-gray-50 p-1 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+          onChange={(e) => {
+            const deviceId = e.target.value;
+            void localVideoTrack
+              .setDevice(deviceId)
+              .then(() => setCurrentCam(deviceId))
+              .catch(console.log);
+          }}
+        >
+          {cams.map((cam) => (
+            <option key={cam.deviceId} selected={cam.deviceId === currentCam} value={cam.deviceId}>
+              {cam.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="mt-2 flex flex-row">
+        <button className="mr-2 h-8" onClick={() => localAudioTrackMute()}>
+          {localAudioMuteState ? <FiMicOff size={25} /> : <FiMic size={25} />}
+        </button>
+        <select
+          id="mic"
+          className="block w-56 truncate rounded-lg border border-gray-300 bg-gray-50 p-1 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+          onChange={(e) => {
+            const deviceId = e.target.value;
+            void localAudioTrack
+              .setDevice(deviceId)
+              .then(() => setCurrentMic(deviceId))
+              .catch(console.log);
+          }}
+        >
+          {mics.map((mic) => (
+            <option
+              className="w-16 truncate"
+              key={mic.deviceId}
+              selected={mic.deviceId === currentMic}
+              value={mic.deviceId}
+            >
+              {mic.label}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
--- src/pages/view.tsx
+++ src/pages/view.tsx
@@ -10,26 +10,31 @@ const ViewRooms = () => {
     return <Card text="loading..." />;
   }
   return (
-    <div className="h-max bg-gray-100 min-h-screen">
-      <h1 className="mb-8 p-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-4xl lg:text-5xl">
+    <div className="h-max min-h-screen bg-gray-100">
+      <h1 className="mb-8 p-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 ">
         Rooms:
       </h1>
-      <div className="flex flex-wrap m-8">
+      <div className="m-8 flex flex-wrap">
         {rooms.data &&
           rooms.data.map((room) => (
-            <div key={room.roomId} className="mb-4 flex flex-wrap justify-center max-w-sm min-w-sm m-auto min-h-96">
+            <div
+              key={room.roomId}
+              className="min-w-sm min-h-96 m-auto mb-4 flex max-w-sm flex-wrap justify-center"
+            >
               <button
-                className="flex cursor-pointer flex-col items-center justify-center self-center rounded bg-white p-4 shadow-sm hover:shadow-lg w-full"
+                className="flex w-full cursor-pointer flex-col items-center justify-center self-center rounded bg-white p-4 shadow-sm hover:shadow-lg"
                 onClick={() => {
                   void router.push(`game/${room.roomId}`);
                 }}
               >
-                <p className="text-xl font-bold dark:text-white">{room.name}</p>
-                <p className="text-md font-normal text-gray-500 dark:text-gray-400 truncate w-56">
-                  {room.description}
-                </p>
+                <p className="text-xl font-bold ">{room.name}</p>
+                <p className="text-md font-normal text-gray-500 ">{room.description}</p>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img className="h-64 w-96 m-auto pb-2 object-cover" src={`/${room.stageName ?? 'stage'}.webp`} alt="stage" />
+                <img
+                  className="m-auto h-64 w-96 object-cover pb-2"
+                  src={`/${room.stageName ?? "stage"}.webp`}
+                  alt="stage"
+                />
               </button>
             </div>
           ))}
